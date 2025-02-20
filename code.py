88bc3f411710e56a4842bc7529de7b719a75b82f
Yasmina1992 <107885151+Yasmina1992@users.noreply.github.com>
2025-02-20 13:09:08 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,4 +2,4 @@ print("Hello, welcome to git!")
 
 #adding nonsense to check updates!!
 
-print("this is the main branch")
+print("this is the not the main branch anymore, we moved to the side branch v1")
